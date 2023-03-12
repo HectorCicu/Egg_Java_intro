0c95d22ce6f5bc1b1938f145c2162411a4bcbce7
Hector Cicutti <hector.cicutti@gmail.com>
2023-03-12 11:02:06 -0300

    q

--- Egg.Java.Inicio/src/Egg/Guia2/Java/Ejercicio_06_Guia2_Extras.java
+++ Egg.Java.Inicio/src/Egg/Guia2/Java/Ejercicio_06_Guia2_Extras.java
@@ -1,7 +1,10 @@
-
 package Egg.Guia2.Java;
 
+import java.util.Scanner;
+
 /**
+ * Leer la altura de N personas y determinar el promedio de estaturas que se
+ * encuentran por debajo de 1.60 mts. y el promedio de estaturas en general.
  *
  * @author Temporal
  */
@@ -11,7 +14,30 @@ public class Ejercicio_06_Guia2_Extras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
 
+        Scanner lee = new Scanner(System.in);
+        int personas, bajo160 = 0;
+        double estatura, alturaGral= 0, alturaBajo = 0;
+
+        System.out.print("Igrese la cantidad de personas a medir: ");
+
+        personas = lee.nextInt();
+
+        for (int i = 1; i <= personas; ++i) {
+            System.out.print("\nAltura de la persona " + i + " --> ");
+            estatura = lee.nextDouble();
+            if (estatura < 1.60) {
+                alturaBajo += estatura;
+                ++bajo160;
+
+            }
+alturaGral += estatura;
+
+        }
+        System.out.println("\nEl promedio general de estatura es : " + (alturaGral / personas));
+        System.out.println("El promedio de estatura de personas menores a 1,60m es: " + (alturaBajo / bajo160));
+        System.out.println("La cantidad de personas de estatura menor a 1,60m es: " + bajo160);
+    }
 }
+
+
---
+++ Egg.Java.Inicio/src/Egg/Guia2/Java/Ejercicio_07_Guia2_Extras1_conWhile.java
@@ -0,0 +1,54 @@
+package Egg.Guia2.Java;
+
+import java.util.Scanner;
+
+/**
+ * Realice un programa que calcule y visualice el valor máximo, el valor mínimo
+ * y el promedio de n números (n>0). El valor de n se solicitará al principio
+ * del programa y los números serán introducidos por el usuario. Realice dos
+ * versiones del programa, una usando el bucle “while” y otra con el bucle “do -
+ * while”.
+ *
+ * @author Temporal
+ */
+public class Ejercicio_07_Guia2_Extras1_conWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner lee = new Scanner(System.in);
+        int totalNum,i = 0;
+        double num, totales = 0, maximo = 0, minimo = 0;
+        
+
+        System.out.print("Ingrese cantidad de números a promediar: ");
+        totalNum = lee.nextInt();
+
+        while (i < totalNum) {
+            ++i;
+            do {
+                System.out.print("Ingrese el valor ( " + i + " de " + totalNum + ")  -> ");
+                num = lee.nextDouble();
+            } while (num <= 0);
+
+            if (i == 1) {
+                maximo = num;
+                minimo = num;
+            }
+            totales += num;
+            if (maximo < num) {
+                maximo = num;
+            }
+            if (minimo > num) {
+                minimo = num;
+            }
+        }
+
+        System.out.println("\nEl valor máximo ingresado es -> " + maximo);
+        System.out.println("El valor mínimo ingresado es -> " + minimo);
+        System.out.println("El promedio de los números ingresados es -> " + (totales / totalNum));
+    }
+
+}
